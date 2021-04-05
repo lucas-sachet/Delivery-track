0d92791bc5124e39414283d98cbdf582b552219d
Lucas Sachet <lucasachet@gmail.com>
2021-04-05 11:33:28 -0300

    feat(route): load positions

---
+++ Simulator/app/route/route.go
@@ -0,0 +1,44 @@
+package route
+
+import "errors"
+
+type Route struct {
+	ID string
+	ClientID string
+	Positions []Position
+}
+
+type Position struct {
+	Lat float64
+	Long float64
+}
+
+// LoadPositions loads from a .txt file all positions (lat and long) to the Position attribute of the struct
+func (r *Route) LoadPositions() error {
+	if r.ID == "" {
+		return errors.New("route id not informed")
+	}
+	f, err := os.Open("destinations/" + r.ID + ".txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), ",")
+		lat, err := strconv.ParseFloat(data[0], 64)
+		if err != nil {
+			return nil
+		}
+		long, err := strconv.ParseFloat(data[1], 64)
+		if err != nil {
+			return nil
+		}
+		r.Positions = append(r.Positions, Position{
+			Lat:  lat,
+			Long: long,
+		})
+	}
+	return nil
+}
+
