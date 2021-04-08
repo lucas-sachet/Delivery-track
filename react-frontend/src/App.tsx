e37e1ba62eabc042816ce58c1a965ad307357364
Lucas Sachet <lucasachet@gmail.com>
2021-04-07 22:31:14 -0300

    feat(react): set up frontend

---
+++ react-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Mapping } from "./components/Mapping";
+
+
+function App() {
+  return (
+    <div>
+      <Mapping/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-frontend/src/components/Mapping.tsx
@@ -0,0 +1,8 @@
+import { Button, Grid, MenuItem, Select } from "@material-ui/core";
+
+type Props = {
+  
+};
+export const Mapping = (props: Props) => {
+  
+};
